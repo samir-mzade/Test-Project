d1bfe63db83d485be94bde6f8813231b02e082e7
samir-mzade <samir.mamedzade211@gmail.com>
2021-12-10 14:09:24 +0400

    new saved file

---
+++ Ploshad i Perimetr Kruqa.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.Write("Vvedite dlinu kruqa");
+int l = Convert.ToInt16(Console.ReadLine());
+double s = l * l * (4 * 3.14);
+double p = l * 3.14;
+Console.WriteLine($"Dlina: {l} Ploshad: {s} Perimetr: {p}");
+
+
+
+
+
+
+
+
